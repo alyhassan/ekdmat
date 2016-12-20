3732d25b6116f950a26f111157ff0d1cef1757be
Aly Hassan <alyhassan.acc@gmail.com>
2016-12-20 23:17:32 +0300

    add logged in user summary function

--- Khadmatcom/Site.Master.cs
+++ Khadmatcom/Site.Master.cs
@@ -10,13 +10,14 @@ using System.Web.UI.WebControls;
 using Khadmatcom.Services;
 using Khadmatcom.Services.Model;
 using Khadmatcom.Services.Services;
+using Microsoft.Ajax.Utilities;
 
 namespace Khadmatcom
 {
     public partial class Site : MasterBase
     {
 
-        public int LanguageId
+        protected int LanguageId
         {
             get
             {
@@ -24,7 +25,24 @@ namespace Khadmatcom
             }
         }
 
-        public string IsoName => GetUiCulture().ToLower();
+        protected int ClientNew = 0;
+        protected int ProviderNew = 0;
+        protected int ClientApproved = 0;
+        protected int ProviderApproved = 0;
+        protected int ClientPaid = 0;
+        protected int ProviderPaid = 0;
+        protected int ClientInProgress = 0;
+        protected int ProviderInProgress = 0;
+        protected int ClinetAccomplished = 0;
+        protected int ProviderAccomplished = 0;
+        protected int ClientRefused = 0;
+        protected int ProviderRefused = 0;
+        protected int ClientCanceled = 0;
+        protected int ProviderCanceled = 0;
+        protected int ClientExpired = 0;
+        protected int ProviderExpired = 0;
+
+        protected string IsoName => GetUiCulture().ToLower();
 
         public string TwoLetterISOLanguageName
         {
@@ -120,8 +138,33 @@ namespace Khadmatcom
             MembershipUser membershipUser = Membership.GetUser();
             var providerUserKey = membershipUser?.ProviderUserKey;
             if (providerUserKey != null)
+            {
                 CurrentUser = userServices.GetUser((Guid)providerUserKey);
+                Khadmatcom.Data.Model.spGetLoggedinSummary_Result currentSummary = _settingServices.GetSummary(CurrentUser.Id);
+                
+                    if (currentSummary.ClientNew.HasValue) ClientNew = currentSummary.ClientNew.Value;
+                    if (currentSummary.ProviderNew.HasValue) ProviderNew = currentSummary.ProviderNew.Value;
+                    if (currentSummary.ClientApproved.HasValue) ClientApproved = currentSummary.ClientApproved.Value;
+                    if (currentSummary.ProviderApproved.HasValue) ProviderApproved = currentSummary.ProviderApproved.Value;
+                    if (currentSummary.ClientPaid.HasValue) ClientPaid = currentSummary.ClientPaid.Value;
+                    if (currentSummary.ProviderPaid.HasValue) ProviderPaid = currentSummary.ProviderPaid.Value;
+                    if (currentSummary.ClientInProgress.HasValue) ClientInProgress = currentSummary.ClientInProgress.Value;
+                    if (currentSummary.ProviderInProgress.HasValue) ProviderInProgress = currentSummary.ProviderInProgress.Value;
+                    if (currentSummary.ClinetAccomplished.HasValue) ClinetAccomplished = currentSummary.ClinetAccomplished.Value;
+                    if (currentSummary.ProviderAccomplished.HasValue) ProviderAccomplished = currentSummary.ProviderAccomplished.Value;
+                    if (currentSummary.ClientRefused.HasValue) ClientRefused = currentSummary.ClientRefused.Value;
+                    if (currentSummary.ProviderRefused.HasValue) ProviderRefused = currentSummary.ProviderRefused.Value;
+                    if (currentSummary.ClientCanceled.HasValue) ClientCanceled = currentSummary.ClientCanceled.Value;
+                    if (currentSummary.ProviderCanceled.HasValue) ProviderCanceled = currentSummary.ProviderCanceled.Value;
+                    if (currentSummary.ClientExpired.HasValue) ClientExpired = currentSummary.ClientExpired.Value;
+                    if (currentSummary.ProviderExpired.HasValue) ProviderExpired = currentSummary.ProviderExpired.Value;
+                
+            }
+
         }
+
+
+
 
         public override void ShowNotifier(string message, string title, NotificationType notificationType)
         {
